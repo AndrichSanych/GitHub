88119b8054755f5bfc763dae6e2b27b67c3f7562
HP <HP@DESKTOP-6RINQJ0>
2023-02-16 19:23:09 +0200

    Add project files.

---
+++ GitHub/GitHub.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<unordered_set>
+
+using namespace std;
+
+template<typename T>
+void printCont(const T& cont)
+{
+	for (auto& i : cont)
+	{
+		cout << "\t" << i;
+	}
+	cout << endl;
+}
+int main()
+{
+	unordered_set<int> s{ 10,5,6,8 };
+	printCont(s);
+    
+    
+}
+
